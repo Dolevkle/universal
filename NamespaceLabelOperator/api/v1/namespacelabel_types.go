33f48798060a7c0d27b6c52a0c13ddbde79b64e6
Dolev Klefner <dolevkle@gmail.com>
2023-11-20 18:07:08 +0000

    [WIP] - reconcile function might be finished didnt check yet

--- NamespaceLabelOperator/api/v1/namespacelabel_types.go
+++ NamespaceLabelOperator/api/v1/namespacelabel_types.go
@@ -23,31 +23,100 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type LabelName string
+
+type NamespacePhase string
+
+type NamespaceConditionType string
+
+type ConditionStatus string
+
+const (
+	ConditionTrue    ConditionStatus = "True"
+	ConditionFalse   ConditionStatus = "False"
+	ConditionUnknown ConditionStatus = "Unknown"
+)
+
+const (
+	// NamespaceDeletionDiscoveryFailure contains information about namespace deleter errors during resource discovery.
+	NamespaceDeletionDiscoveryFailure NamespaceConditionType = "NamespaceDeletionDiscoveryFailure"
+	// NamespaceDeletionContentFailure contains information about namespace deleter errors during deletion of resources.
+	NamespaceDeletionContentFailure NamespaceConditionType = "NamespaceDeletionContentFailure"
+	// NamespaceDeletionGVParsingFailure contains information about namespace deleter errors parsing GV for legacy types.
+	NamespaceDeletionGVParsingFailure NamespaceConditionType = "NamespaceDeletionGroupVersionParsingFailure"
+	// NamespaceContentRemaining contains information about resources remaining in a namespace.
+	NamespaceContentRemaining NamespaceConditionType = "NamespaceContentRemaining"
+	// NamespaceFinalizersRemaining contains information about which finalizers are on resources remaining in a namespace.
+	NamespaceFinalizersRemaining NamespaceConditionType = "NamespaceFinalizersRemaining"
+)
+
+const (
+	// NamespaceActive means the namespace is available for use in the system
+	NamespaceActive NamespacePhase = "Active"
+	// NamespaceTerminating means the namespace is undergoing graceful termination
+	NamespaceTerminating NamespacePhase = "Terminating"
+)
+
+const (
+	FinalizerKubernetes LabelName = "kubernetes"
+)
+
+type NamespaceCondition struct {
+	// Type of namespace controller condition.
+	Type NamespaceConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=NamespaceConditionType"`
+	// Status of the condition, one of True, False, Unknown.
+	Status ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=ConditionStatus"`
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,4,opt,name=lastTransitionTime"`
+	// +optional
+	Reason string `json:"reason,omitempty" protobuf:"bytes,5,opt,name=reason"`
+	// +optional
+	Message string `json:"message,omitempty" protobuf:"bytes,6,opt,name=message"`
+}
+
 // NamespaceLabelSpec defines the desired state of NamespaceLabel
 type NamespaceLabelSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of NamespaceLabel. Edit namespacelabel_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Labels map[string]string `json:"labels,omitempty" protobuf:"bytes,1,rep,name=labels,casttype=FinalizerName"`
 }
 
 // NamespaceLabelStatus defines the observed state of NamespaceLabel
 type NamespaceLabelStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Phase is the current lifecycle phase of the namespace.
+	// More info: https://kubernetes.io/docs/tasks/administer-cluster/namespaces/
+	// +optional
+	Phase NamespacePhase `json:"phase,omitempty" protobuf:"bytes,1,opt,name=phase,casttype=NamespacePhase"`
+
+	// Represents the latest available observations of a namespace's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []NamespaceCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,2,rep,name=conditions"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
 
 // NamespaceLabel is the Schema for the namespacelabels API
 type NamespaceLabel struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Spec defines the behavior of the Namespace.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+	// +optional
+	Spec NamespaceLabelSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
 
-	Spec   NamespaceLabelSpec   `json:"spec,omitempty"`
-	Status NamespaceLabelStatus `json:"status,omitempty"`
+	// Status describes the current status of a Namespace.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+	// +optional
+	Status NamespaceLabelStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
 }
 
 //+kubebuilder:object:root=true
--- NamespaceLabelOperator/internal/controller/namespacelabel_controller.go
+++ NamespaceLabelOperator/internal/controller/namespacelabel_controller.go
@@ -18,7 +18,11 @@ package controller
 
 import (
 	"context"
+	"reflect"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -47,11 +51,84 @@ type NamespaceLabelReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *NamespaceLabelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	nsLabel := &multitenancyv1.NamespaceLabel{}
+
+	log.Info("Reconciling namespacelabel")
+
+	// Fetch the namespacelabel instance
+	if err := r.Get(ctx, req.NamespacedName, nsLabel); err != nil {
+		// namespacelabel object not found, it might have been deleted
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	ns := &corev1.Namespace{}
+
+	log.Info("Ensuring Namespace", "namespace", nsLabel.Name)
+
+	// Fetch the corresponding Namespace
+	// Define a ns object
+	if err := r.ensureNamespace(ctx, nsLabel, ns); err != nil {
+		log.Error(err, "unable to ensure Namespace", "namespace", ns)
+		return ctrl.Result{}, err
+	}
+
+	// Reconcile Namespace labels
+	lbls := nsLabel.Spec.Labels
+
+	if lbls == nil {
+		lbls = make(map[string]string)
+	}
+
+	log.Info("synchronizing namespace labels with NamespaceLabels")
+
+	if !reflect.DeepEqual(ns.Labels, lbls) {
+		ns.Labels = lbls
+		if err := r.Update(ctx, ns); err != nil {
+			log.Error(err, "unable to update Namespace labels")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Namespace labels reconciled")
+	}
 
 	return ctrl.Result{}, nil
+}
+
+// ensureNamespace is a function that ensures that namespace exists,
+// else it creates one.
+func (r *NamespaceLabelReconciler) ensureNamespace(ctx context.Context, nsLabel *multitenancyv1.NamespaceLabel, ns *corev1.Namespace) error {
+	log := log.FromContext(ctx)
+
+	namespaceName := nsLabel.Name
+
+	// Attempt to get the namespace with the provided name
+	err := r.Get(ctx, client.ObjectKey{Name: namespaceName}, ns)
+	if err != nil {
+		// If the namespace doesn't exist, create it
+		if errors.IsNotFound(err) {
+			log.Info("Creating Namespace", "namespace", namespaceName)
+			namespace := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   namespaceName,
+					Labels: nsLabel.Spec.Labels,
+				},
+			}
+			// Attempt to create the namespace
+			if err = r.Create(ctx, namespace); err != nil {
+				log.Error(err, "unable to create Namespace")
+				return err
+			}
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		// If the namespace already exists, check for required annotations
+		log.Info("Namespace already exists", "namespace", namespaceName)
+		return nil
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
